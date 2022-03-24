3e6d22320f2e4a060ab1eac9f3f842ea6426ec78
jonyprill <jonathantprill@gmail.com>
2022-03-24 15:13:58 -0500

    created more handlebar views, got a working home page, login link is broken

--- server.js
+++ server.js
@@ -1,37 +1,49 @@
-const path = require('path');
 const express = require('express');
-// const session = require('express-session');
 const exphbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const app = express();
+//This uses Heroku's process.env.PORT value when deployed and 3001 when run locally.
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
-
-// app.use(session(sess));
-
-const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+
+// pass the helpers 
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+//handlebars
+
+//Cookies
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+//Cookies
+
+//This method is for specific front end files like style sheets, and js files. 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+// turn on routes
+app.use(routes);
 
+
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
